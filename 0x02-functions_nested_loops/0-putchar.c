70c642009c0e9d9fc1e9583200ec121ceeaaae31
justaddisu <justaddisu@gmail.com>
2022-03-16 06:44:17 -0700

    task 0 file modified

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,16 @@
 #include "main.h"
-
 /**
  * main - program that prints _putchar.
  *
  * Return 0
- */
+ **/
 
 int main(void)
 {	
 	char addisu[] = "_putchar";	
+	
 	int c;
+	
 	for(c = 0; c < 8; c++)
 	{
 		_putchar(addisu[c]);
