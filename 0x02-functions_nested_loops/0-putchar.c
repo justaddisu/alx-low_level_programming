a3bb7a552dfe2cca298c619269a32155a1c080e3
justaddisu <justaddisu@gmail.com>
2022-03-16 06:33:04 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,20 @@
 #include "main.h"
+
 /**
- * main - prints the string "_putchar" from a character array.
- * Return: 0 on success.
+ * main - program that prints _putchar.
+ *
+ * Return: int
  */
+
 int main(void)
 {
-	char c[] = "_putchar";
-	int i;
+	int n = 0;
+	char str_hbt[9] = "_putchar";
 
-	for (i = 0; i < 10; i++)
+	while (n < 9)
 	{
-		_putchar(c[i]);
+		_putchar(str_hbt[n]);
+		n += 1;
 	}
 	_putchar('\n');
 	return (0);
