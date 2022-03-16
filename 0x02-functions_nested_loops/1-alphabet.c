37607f7cc449d5918c56600aff2d2b8a0627ef22
justaddisu <justaddisu@gmail.com>
2022-03-16 09:06:27 -0700

    task 1 to print a-z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,19 @@
 #include "main.h"
+
 /**
- * main - check the code
+ * main - check the code.
  *
  * Return: Always 0.
  */
-void print_alphabet(void){
 
-    char c = 'a';
-    for(c = 'a'; c <= 'z'; c++){    
-        _putchar(c);
-    }
-    _putchar('\n');       
+void print_alphabet_x10(void)
+{
+	int l = 'a';
+
+	while (l <= 'z')
+	{
+		_putchar(l);
+		l += 1;
+	}
+	_putchar(10);
 }
