a15e387433743c203478081d80cf45552786f7ad
justaddisu <justaddisu@gmail.com>
2022-03-21 10:35:10 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * puts2 - prints one char out of 2
- * @str: type
+ *puts2 - prints every character of a string
+ *@str: A pointer to an int that will be changed
  *
+ *Return: void which means it is correct
+ */
 
 void puts2(char *str)
 {
